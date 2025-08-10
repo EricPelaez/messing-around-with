1414d41e65de1c8cc4da9a84c80e57e453526aab
EricPelaez <legodudeboom@gmail.com>
2025-08-09 19:40:28 -0700

    completed terreaform-4

---
+++ terraform/terraform-4/resources.tf
@@ -0,0 +1,49 @@
+resource "aws_security_group" "web_app_sg" {
+  name        = "web_app_sg"
+  description = "web app  security group"
+  vpc_id      = data.aws_vpc.default.id
+
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "from_terraform"
+  }
+}
+
+
+resource "aws_instance" "web_app_instance" {
+  ami = var.ami
+  subnet_id = data.aws_subnet.default.id
+  vpc_security_group_ids = [aws_security_group.web_app_sg.id]
+  instance_type = var.instance_type
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Welcome to the Terraform Web App</h1>" > /var/www/html/index.html
+              EOF
+  tags = {
+    Name = "web-app"
+  }
+}
